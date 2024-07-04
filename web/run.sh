48f941afa6f90d0a413eebfff33077920d72dc0d
ayeaaaa <153930183+ayeaaaa@users.noreply.github.com>
2024-07-04 13:52:05 +0800

    Update run.sh

--- web/run.sh
+++ web/run.sh
@@ -1,3 +1,25 @@
+export TZ="Asia/Shanghai"
+# 获取当前脚本所在的目录
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+
+# 重新加载当前 Shell 的配置，以便使用 nvm
+source ~/.bashrc
+
+# 加载 nvm 环境设置
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# 检查 nvm 是否成功加载
+if command -v nvm &> /dev/null; then
+    echo "nvm 已加载。"
+else
+    echo "无法找到 nvm 命令，请检查安装过程中的问题。"
+    exit 1  # 如果无法找到 nvm 命令，退出脚本并返回错误码
+fi
+
+
 # 询问用户操作选项
 echo "请选择一个操作："
 echo "1. 启动服务"
